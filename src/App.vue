7e7e41e7a5b12d772b8bc863c57fc2ba7f2b583d
조석규 <ezaurum@gmail.com>
2018-10-07 23:40:39 +0900

    Update component for slots

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
     <div class="div2">
       이 클래스는 위로 올라갈 것
     </div>
-    <oc></oc>
+    <oc class="wrapper"
+        background="yellow"
+        overlay="top"
+        :opacity="0.4">
+      <div class="menu">
+        오버레이 메뉴
+      </div>
+    </oc>
     <div class="div1">
       이 클래스는 뒤로 들어갈 것
     </div>
@@ -19,14 +26,30 @@ export default {
 }
 </script>
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 .div1 {
   &:after {
     content: '캔버스보다 위';
   }
   background-color: red;
-  width:100%;
-  height:40px;
-  color:white;
+  width: 100%;
+  height: 40px;
+  color: white;
+  position: absolute;
+}
+
+.wrapper {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  width: 200px;
+  height: 300px;
+  border-radius: 5px;
+  background: #c0c0c0;
 }
 
 .div2 {
@@ -34,8 +57,8 @@ export default {
     content: '캔버스보다 아래';
   }
   background-color: blue;
-  width:100%;
-  height:40px;
-  color:white;
+  width: 100%;
+  height: 40px;
+  color: white;
 }
 </style>
--- src/components/OverlayCanvas.vue
+++ src/components/OverlayCanvas.vue
@@ -1,14 +1,94 @@
 <template>
   <div class="canvas-wrapper">
     <canvas></canvas>
+    <div v-if="hasSlot" :class="canvasOverlayClass">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
 
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 300,
+    },
+    height: {
+      type: Number,
+      default: 300,
+    },
+    opacity: {
+      type: Number,
+      default: 1,
+    },
+    background: {
+      type: String,
+      default: 'transparent',
+    },
+    overlay: {
+      type: String,
+      default: 'bottom',
+    },
+  },
+  computed: {
+    hasSlot() {
+      if (!this.$slots.default) return false
+      if (!this.$slots.default[0].text) return false
+      if (!this.$slots.default[0].text.length) return false
+      return !this.$slots.default[0].text.length
+    },
+    canvasOverlayClass() {
+      return {
+        'canvas-wrapper__overlay': true,
+        'canvas-wrapper__overlay--bottom': this.overlay === 'bottom',
+        'canvas-wrapper__overlay--top': this.overlay === 'top',
+        'canvas-wrapper__overlay--left': this.overlay === 'left',
+        'canvas-wrapper__overlay--right': this.overlay === 'right',
+      }
+    },
+  },
+  data() {
+    return {
+      canvas: Object,
+      ctx: Object,
+    }
+  },
+  mounted() {
+    let canvasElement = this.$el.querySelector('canvas')
+    let ctx = canvasElement.getContext('2d')
+    this.canvas = canvasElement
+    this.ctx = ctx
+    this.canvas.style['background'] = this.background
+    this.canvas.style['opacity'] = this.opacity
+    this.resetSize(this.width, this.height)
+  },
+  watch: {
+    width(to, from) {
+      const width = to
+      this.resetSize(width, this.height)
+    },
+    height(to, from) {
+      const height = to
+      this.resetSize(this.width, height)
+    },
+  },
+  methods: {
+    resetSize(width, height) {
+      this.canvas.width = width
+      this.canvas.height = height
+    },
+  },
+}
+
 </script>
-<style>
-canvas {
-  background-color: red;
-  opacity: 0.5;
+<style lang="scss" itemscope>
+.canvas-wrapper {
+  position: relative;
+  overflow: hidden;
+}
+
+.canvas-wrapper__overlay {
+
 }
 </style>
